37638f3128d1abd68aa56ca657fe167f8c9776cb
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-06-06 17:58:11 +0200

    feat: use single object for theme settings

--- theme/fonts.js
+++
@@ -1,4 +0,0 @@
-export default {
-  body: '"Raleway", -apple-system, BlinkMacSystemFont, sans-serif',
-  heading: '"Montserrat", -apple-system, BlinkMacSystemFont, sans-serif',
-};
--- theme/index.js
+++ theme/index.js
@@ -1,7 +1,46 @@
-import colors from './colors';
-import fonts from './fonts';
-
 export default {
-  colors,
-  fonts,
+  colors: {
+    blue: {
+      default: '#3A89C9',
+      dark: '#1B325F',
+      light: '#9CC4E4',
+    },
+    gray: {
+      light: '#F2F2F2',
+      medium: '#C4C4C4',
+    },
+    black: '#000000',
+    white: '#FFFFFF',
+    orange: '#F26C4F',
+    red: '#FF2047',
+  },
+
+  fonts: {
+    body: '"Raleway", -apple-system, BlinkMacSystemFont, sans-serif',
+    heading: '"Montserrat", -apple-system, BlinkMacSystemFont, sans-serif',
+  },
+
+  fontSizes: [0.5, 0.707, 1, 1.414, 1.999, 2.827, 3.998, 5.653].map(value => `${value}rem`),
+
+  lineHeights: {
+    body: 1.65,
+    heading: 1.15,
+  },
+
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+
+  // Fibonacci scale :)
+  space: {
+    xxs: '0.25rem',
+    xs: '0.5rem',
+    sm: '0.75rem',
+    md: '1.25rem',
+    lg: '2rem',
+    xl: '3.25',
+    xxl: '5.25rem',
+  },
 };
