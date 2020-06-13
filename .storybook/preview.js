419f0711c1c6e6b8db3e180640accaf61356fc44
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-06-13 23:32:10 +0200

    feat: add global styles to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,10 +3,21 @@ import { addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from '../theme';
+import GlobalStyle from '../theme/global-style';
+
+const containerStyles = {
+  maxWidth: '1440px',
+  minHeight: '100vh',
+  padding: '15px',
+  position: 'relative',
+  margin: '0 auto',
+  width: '100%',
+};
 
 const ThemeDecorator = StoryFn => (
   <ThemeProvider theme={theme}>
-    <div style={{ minHeight: '100vh', padding: '15px', position: 'relative' }}>
+    <GlobalStyle />
+    <div style={containerStyles}>
       <StoryFn />
     </div>
   </ThemeProvider>
