be380d48219708e883dda93dfc47ff41e950c695
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-08-05 00:08:44 +0200

    feat: use layout component in _app.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,3 @@
-import Layout from '../components/layout';
 import { getAllPosts } from '../lib/posts';
 import Heading from '../components/primitives/heading';
 import Container from '../components/container';
@@ -6,24 +5,22 @@ import PostTeaser from '../components/post-teaser';
 
 export default function BlogPage({ allPostsData }) {
   return (
-    <Layout>
-      <Container>
-        <Heading textColor="primary">Blog</Heading>
+    <Container>
+      <Heading textColor="primary">Blog</Heading>
 
-        <div>
-          {allPostsData.length ? (
-            allPostsData.map(post => <PostTeaser post={post} key={post.id} showReadmore />)
-          ) : (
-            <p>
-              I haven't written any post yet.{' '}
-              <span role="img" aria-label="sad">
-                &#128532;
-              </span>
-            </p>
-          )}
-        </div>
-      </Container>
-    </Layout>
+      <div>
+        {allPostsData.length ? (
+          allPostsData.map(post => <PostTeaser post={post} key={post.id} showReadmore />)
+        ) : (
+          <p>
+            I haven't written any post yet.{' '}
+            <span role="img" aria-label="sad">
+              &#128532;
+            </span>
+          </p>
+        )}
+      </div>
+    </Container>
   );
 }
 
