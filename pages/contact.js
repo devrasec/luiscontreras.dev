b12b2c2e61d12ec64d81577cc0b1f2abb92e5122
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-08-15 22:48:37 +0200

    feat: remove contact form option

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,8 @@
 import Container from '../components/container';
 import Heading from '../components/primitives/heading';
+import websiteConfig from '../config/website';
+
+const { socialLinks } = websiteConfig;
 
 export default function ContactPage() {
   return (
@@ -14,19 +17,18 @@ export default function ContactPage() {
       <ul>
         <li>
           DM me on Twitter{' '}
-          <a href="https://twitter.com/devrasec/" target="_blank" rel="noopener noreferrer">
+          <a href={socialLinks.twitter} target="_blank" rel="noopener noreferrer">
             @devrasec
           </a>{' '}
-          (preferably)
+          (preferably) or Instagram{' '}
+          <a href={socialLinks.instagram} target="_blank" rel="noopener noreferrer">
+            @luiscontreras.dev
+          </a>
         </li>
 
         <li>
           Send me an email to <a href="mailto:luis@luiscontreras.dev">luis@luiscontreras.dev</a>
         </li>
-
-        <li>Or send me a message by filling out the form below.</li>
-
-        {/* TODO: add a contact form here */}
       </ul>
     </Container>
   );
