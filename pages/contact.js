8599f810cdd71733ec6640e1eb86ae34c2453a2a
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-08-15 14:45:01 +0200

    feat: add content to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,29 @@ export default function ContactPage() {
   return (
     <Container as="main" role="main">
       <Heading textColor="primary">Contact</Heading>
+
+      <p>
+        Hey! Looking to get in touch? don't be affraid to contact me!. If you'd like to contact me,
+        here is how you can do it:
+      </p>
+
+      <ul>
+        <li>
+          DM me on Twitter{' '}
+          <a href="https://twitter.com/devrasec/" target="_blank" rel="noopener noreferrer">
+            @devrasec
+          </a>{' '}
+          (preferably)
+        </li>
+
+        <li>
+          Send me an email to <a href="mailto:luis@luiscontreras.dev">luis@luiscontreras.dev</a>
+        </li>
+
+        <li>Or send me a message by filling out the form below.</li>
+
+        {/* TODO: add a contact form here */}
+      </ul>
     </Container>
   );
 }
