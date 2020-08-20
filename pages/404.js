a16cab82e98e18173e2dba61165e87afb5cc14b5
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-08-20 23:50:25 +0200

    feat: create 404 page

---
+++ pages/404.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import Container from '../components/container';
+import Heading from '../components/primitives/heading';
+
+export default function Page404() {
+  return (
+    <Container>
+      <Heading
+        textColor="primary"
+        looksLike="h3"
+        css={`
+          text-transform: uppercase;
+        `}
+      >
+        404 Page Not Found
+      </Heading>
+
+      <p>Ooops, you're in the wrong place.</p>
+      <p>
+        Go to the{' '}
+        <Link href="/">
+          <a>homepage</a>
+        </Link>
+      </p>
+      <p>
+        If you think this is not right, please{' '}
+        <Link href="/contact">
+          <a>contact me</a>
+        </Link>
+      </p>
+    </Container>
+  );
+}
