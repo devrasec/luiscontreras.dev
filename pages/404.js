4f44db55722ed6e0513078ee7274194e868ce83f
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-08-27 23:12:24 +0200

    feat: add title to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,33 +1,37 @@
 import Link from 'next/link';
+import { NextSeo } from 'next-seo';
 import Container from '../components/container';
 import Heading from '../components/primitives/heading';
 
 export default function Page404() {
   return (
-    <Container>
-      <Heading
-        textColor="primary"
-        looksLike="h3"
-        css={`
-          text-transform: uppercase;
-        `}
-      >
-        404 Page Not Found
-      </Heading>
+    <>
+      <NextSeo title="Not Found" />
+      <Container>
+        <Heading
+          textColor="primary"
+          looksLike="h3"
+          css={`
+            text-transform: uppercase;
+          `}
+        >
+          404 Page Not Found
+        </Heading>
 
-      <p>Ooops, you're in the wrong place.</p>
-      <p>
-        Go to the{' '}
-        <Link href="/">
-          <a>homepage</a>
-        </Link>
-      </p>
-      <p>
-        If you think this is not right, please{' '}
-        <Link href="/contact">
-          <a>contact me</a>
-        </Link>
-      </p>
-    </Container>
+        <p>Ooops, you're in the wrong place.</p>
+        <p>
+          Go to the{' '}
+          <Link href="/">
+            <a>homepage</a>
+          </Link>
+        </p>
+        <p>
+          If you think this is not right, please{' '}
+          <Link href="/contact">
+            <a>contact me</a>
+          </Link>
+        </p>
+      </Container>
+    </>
   );
 }
