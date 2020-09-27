b6ad4ca1305a6bfe1ca969b1b93e1955c24c60e7
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-09-27 21:52:33 +0200

    feat: update logo and add icons

--- pages/_document.js
+++ pages/_document.js
@@ -37,6 +37,13 @@ export default class SiteDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f26c4f" />
+          <meta name="msapplication-TileColor" content="#f26c4f" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
