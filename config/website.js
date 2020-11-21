d3761c105308cebb7909d73e5eb609932d796914
Luis Contreras <897080+devrasec@users.noreply.github.com>
2020-11-22 00:00:01 +0100

    fix: set fixed site domain as siteUrl

--- config/website.js
+++ config/website.js
@@ -2,7 +2,8 @@ const websiteConfig = {
   pageTitle: 'Luis Contreras',
   pageDescription:
     'Luis Contreras, Software Engineer in Stockholm. Sharing my knowledge of Web Development.',
-  siteUrl: process.env.ENV === 'production' ? 'https://luiscontreras.dev' : 'http://localhost:3000',
+  // siteUrl: process.env.ENV === 'production' ? 'https://luiscontreras.dev' : 'http://localhost:3000',
+  siteUrl: 'https://luiscontreras.dev', // TODO: this should be environment aware.
   socialLinks: {
     twitter: 'https://twitter.com/devrasec/',
     github: 'https://github.com/devrasec/',
