872ce98965c1c211216bfb2cf940133d49e2730c
Luis Contreras <one.tmac@gmail.com>
2020-12-22 22:55:04 +0100

    Fix image sizes (#93)
    * chore(deps): update some dependencies
    
    * fix: correctly setup image sizes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     config.module.rules.push({ test: /\.mdx$/, use: 'raw-loader' });
     return config;
   },
+  images: {
+    deviceSizes: [360, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
 
 module.exports = nextConfig;
